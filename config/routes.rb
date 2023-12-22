47edddcec6a4c415513054831f16a02e718be072
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-12-22 09:31:28 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
 end
   resources :inventories
   resources :foods
-  resources :users
+  # resources :users
 
-  root 'users#index'
+  root 'inventories#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
