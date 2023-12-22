b7a44c60c1a0445399c34d8ee2ef7bf64aef29d8
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-22 07:44:14 +0500

    Design the Inventories List page

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,73 +1,35 @@
 class InventoryFoodsController < ApplicationController
-  before_action :set_inventory_food, only: %i[show edit update destroy]
-
-  # GET /inventory_foods or /inventory_foods.json
-  def index
-    @inventory_foods = InventoryFood.all
-  end
-
-  # GET /inventory_foods/1 or /inventory_foods/1.json
-  def show; end
-
-  # GET /inventory_foods/new
   def new
-    @inventory_food = InventoryFood.new
+    @user = current_user
+    # check if I am receiving the inventory_id in the params
+    @inventory = Inventory.find(params[:inventory_id])
+    @inventory_food = @inventory.inventory_foods.new
+    @foods = Food.all
   end
 
-  # GET /inventory_foods/1/edit
-  def edit; end
-
-  # POST /inventory_foods or /inventory_foods.json
   def create
-    @inventory_food = InventoryFood.new(inventory_food_params)
-
-    respond_to do |format|
-      if @inventory_food.save
-        format.html do
-          redirect_to inventory_food_url(@inventory_food), notice: 'Inventory food was successfully created.'
-        end
-        format.json { render :show, status: :created, location: @inventory_food }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @inventory_food.errors, status: :unprocessable_entity }
-      end
+    @inventory = Inventory.find(params[:inventory_id])
+    @inventory_food = @inventory.inventory_foods.new(food_id: params[:food][:food_id], quantity: params[:quantity])
+    if @inventory_food.save
+      redirect_to inventory_path(@inventory), notice: 'Inventory food successfully created.'
+    else
+      render :new, alert: 'Inventory food not created.'
     end
   end
 
-  # PATCH/PUT /inventory_foods/1 or /inventory_foods/1.json
-  def update
-    respond_to do |format|
-      if @inventory_food.update(inventory_food_params)
-        format.html do
-          redirect_to inventory_food_url(@inventory_food), notice: 'Inventory food was successfully updated.'
-        end
-        format.json { render :show, status: :ok, location: @inventory_food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @inventory_food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /inventory_foods/1 or /inventory_foods/1.json
   def destroy
-    @inventory_food.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to inventory_foods_url, notice: 'Inventory food was successfully destroyed.' }
-      format.json { head :no_content }
+    @inventory_food = InventoryFood.find(params[:id])
+    @inventory = @inventory_food.inventory
+    if @inventory_food.destroy
+      redirect_to inventory_path(@inventory), notice: 'Inventory food successfully deleted.'
+    else
+      redirect_to inventory_path(@inventory), alert: 'Inventory food not deleted.'
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_inventory_food
-    @inventory_food = InventoryFood.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def inventory_food_params
-    params.require(:inventory_food).permit(:inventory_id, :food_id, :quantity)
+    params.require(:inventory_foods).permit(:quantity, :food_id)
   end
 end
--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,78 +1,52 @@
 class RecipeFoodsController < ApplicationController
-  before_action :set_recipe_food, only: %i[show edit update destroy]
-
-  # GET /recipe_foods or /recipe_foods.json
-  def index
-    @recipe_foods = RecipeFood.all
-  end
-
-  # GET /recipe_foods/1 or /recipe_foods/1.json
-  def show; end
-
-  # GET /recipe_foods/new
   def new
-    @recipe_food = RecipeFood.new
+    @user = current_user
+    # check if I am receiving the recipe_id in the params
     @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new
+    @foods = Food.all
   end
 
-  # GET /recipe_foods/1/edit
-  def edit
-    @recipe_food = RecipeFood.find(params[:id])
-    @recipe = Recipe.find(params[:recipe_id])
-    @edit = true
-  end
-
-  # POST /recipe_foods or /recipe_foods.json
   def create
-    # @recipe_food = RecipeFood.new(recipe_food_params)
-    @food = Food.find_by(name: params[:recipe_food][:food_name])
-    if @food
-
-      @quantity = @food.quantity - (params[:recipe_food][:quantity]).to_i
-      @food.update(quantity: @quantity)
-      @recipe_food = RecipeFood.new(quantity: params[:recipe_food][:quantity])
-      @recipe = Recipe.find(params[:recipe_food][:recipe_id])
-      @recipe_food.recipe = @recipe
-      @recipe_food.food = @food
-      render :new, status: :unprocessable_entity unless @recipe_food.save
-    else
-      redirect_to new_recipe_food_url(@recipe_food), alert: 'Food not available, Add it first.'
-    end
-  end
-
-  # PATCH/PUT /recipe_foods/1 or /recipe_foods/1.json
-  def update
-    @recipe_food = RecipeFood.find_by(params[:recipe_id])
-    @old_quantity = @recipe_food.quantity
-
-    @new_quantity = if @old_quantity > (params[:recipe_food][:quantity]).to_i
-                      @old_quantity - (params[:recipe_food][:quantity]).to_i
-                    else
-                      (params[:recipe_food][:quantity]).to_i - @old_quantity
-                    end
-    @recipe_food.update(quantity: params[:recipe_food][:quantity])
-    @food = Food.find(@recipe_food.food_id)
-    @quantity = @food.quantity - @new_quantity
-    @food.update(quantity: @quantity)
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new(food_id: params[:food][:food_id], quantity: params[:quantity])
     if @recipe_food.save
-      redirect_to recipe_url(@recipe_food.recipe.id), notice: 'Igredient quantity was successfully updated.'
+      redirect_to recipe_path(@recipe), notice: 'Recipe food successfully created.'
     else
-      render :new, status: :unprocessable_entity
+      render :new, alert: 'Recipe food not created.'
     end
   end
 
-  # DELETE /recipe_foods/1 or /recipe_foods/1.json
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
-    @recipe = Recipe.find(params[:recipe_id])
-    @food = Food.find(@recipe_food.food_id)
-    @quantity = @food.quantity + @recipe_food.quantity
-    @food.update(quantity: @quantity)
-    nil unless @recipe_food.destroy
+    @recipe = @recipe_food.recipe
+    if @recipe_food.destroy
+      redirect_to recipe_path(@recipe), notice: 'Recipe food successfully deleted.'
+    else
+      redirect_to recipe_path(@recipe), alert: 'Recipe food not deleted.'
+    end
   end
-  redirect_to recipe_url(@recipe), notice: 'Recipe ingredient was successfully removed.'
-end
+
+  def edit
+    @user = current_user
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe = @recipe_food.recipe
+    @foods = Food.all
+  end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe = @recipe_food.recipe
+    if @recipe_food.update(recipe_food_params)
+      redirect_to recipe_path(@recipe), notice: 'Recipe food successfully updated.'
+    else
+      redirect_to request.referrer, alert: 'Recipe food not updated.'
+    end
+  end
 
   private
 
-params.require(:recipe_food).permit(:quantity, :food_name, :recipe_id)
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :food_id)
+  end
+end
