c176fe7dbd70bb67df44dcd1bd3f7e9ec14f7a2a
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-23 16:47:07 +0500

    Remove linters errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,7 @@ class Food < ApplicationRecord
   has_many :inventory_foods, dependent: :destroy
   has_many :recipe_foods, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 250}
+  validates :name, presence: true, length: { maximum: 250 }
   validates :measurement_unit, presence: true
-  validates :price, presence: true, numericality: {greater_than: 0}
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
